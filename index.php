02e951837619c3197668a79a0972bae4a345b556
ivanaandrasek <49304038+ivanaandrasek@users.noreply.github.com>
2019-04-05 10:24:35 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Zdravo</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="zdravo.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+</head>
+<body>
+
+	<h1>Zdravo</h1><h3>Naruči klopu, brzo i fino!</h3>
+	<hr>
+
+	<form method="post" action="">
+
+	<table>
+		<tr>
+		
+		<td align="right"><h4>Menu</h4></td>
+		<td width="700px" align="right"><select name="menu">
+			<option>Mješana pzza</option>
+			<option>Šunka - sir pizza</option>
+			<option>Margarita pizza</option>
+			<option>Slavonska pizza</option>
+			<option>Sportska pizaa</option>
+		</select></td>
+		</tr>
+
+		<tr>
+		<td align="right"><h4>Ime</h4></td>
+		<td width="700px" align="right"><input type="text" name="ime"></td>
+		</tr>
+
+		<tr>
+		<td align="right"><h4>Adresa</h4></td>
+		<td width="700px" align="right"><textarea name="adresa"></textarea></td>
+		</tr>
+		</table>
+
+		<input type="submit" value="Naruči">&nbsp
+		<input type="reset" value="Obriši">
+
+	</form>
+
+	<br>
+
+
+</body>
+</html>
